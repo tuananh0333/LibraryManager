a9fb10db9130171931bfb78f620a3beb4105ba08
Huy Phan <enpiech@gmail.com>
2019-05-19 16:24:34 +0700

    Downgrade to 3.3

--- app/src/main/java/com/example/librarymanager/ViewHolders/CategoryViewHolderAbstract.java
+++ app/src/main/java/com/example/librarymanager/ViewHolders/CategoryViewHolderAbstract.java
@@ -9,22 +9,16 @@ import com.example.librarymanager.R;
 import com.example.librarymanager.models.CategoryModel;
 
 public class CategoryViewHolderAbstract extends AbstractCustomViewHolder {
-    private ImageView categoryImage;
     private TextView categoryName;
 
     public CategoryViewHolderAbstract(@NonNull View itemView) {
         super(itemView);
-
-        categoryImage = itemView.findViewById(R.id.imgCategory);
         categoryName = itemView.findViewById(R.id.txtCategory);
     }
 
     @Override
     public void setData(Object data) {
         CategoryModel category = (CategoryModel) data;
-
-        // TODO get image from firebase
-
         this.categoryName.setText(category.getName());
     }
 }
